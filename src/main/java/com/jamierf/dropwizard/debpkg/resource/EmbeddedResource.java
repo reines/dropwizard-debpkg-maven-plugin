384d30fe0e671e25f8fcfb77b78556b46a208e64
Jamie Furness <jfurness@yammer-inc.com>
2014-10-13 19:49:03 +0100

    Removing unused equality methods

--- src/main/java/com/jamierf/dropwizard/debpkg/resource/EmbeddedResource.java
+++ src/main/java/com/jamierf/dropwizard/debpkg/resource/EmbeddedResource.java
@@ -21,24 +21,4 @@ public class EmbeddedResource extends Resource {
     public ByteSource getSource() {
         return Resources.asByteSource(resource);
     }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        final EmbeddedResource that = (EmbeddedResource) o;
-
-        if (resource != null ? !resource.equals(that.resource) : that.resource != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (resource != null ? resource.hashCode() : 0);
-        return result;
-    }
 }
--- src/main/java/com/jamierf/dropwizard/debpkg/resource/FileResource.java
+++ src/main/java/com/jamierf/dropwizard/debpkg/resource/FileResource.java
@@ -23,24 +23,4 @@ public class FileResource extends Resource {
     public ByteSource getSource() {
         return Files.asByteSource(file);
     }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        final FileResource that = (FileResource) o;
-
-        if (file != null ? !file.equals(that.file) : that.file != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (file != null ? file.hashCode() : 0);
-        return result;
-    }
 }
