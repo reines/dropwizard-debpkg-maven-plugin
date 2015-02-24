f1c2e68ec54a577f72228ef1cabaa2525108ee71
Martin Charlesworth <martin.charlesworth@netnames.com>
2015-02-24 12:10:07 +0000

    fixed more tab formatting

--- src/main/java/com/jamierf/dropwizard/debpkg/config/DebConfiguration.java
+++ src/main/java/com/jamierf/dropwizard/debpkg/config/DebConfiguration.java
@@ -43,7 +43,7 @@ public class DebConfiguration {
     }
     
     public DebConfiguration setName(String name) {
-		this.name = name;
-		return this;
-	}
+        this.name = name;
+        return this;
+    }
 }
